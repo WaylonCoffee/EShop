be7a344a29cbc83ad6210a85ed0d86ca4d7d1494
charlie-peng@champs.cn <peng456>
2020-10-26 14:51:54 +0800

    init

---
+++ wave/wave-product/src/main/java/com/qingala/admin/category/Category.java
@@ -0,0 +1,24 @@
+package com.qingala.admin.category;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class Category {
+
+    @NotNull(message = "分类ID不能为空")
+    private String id;
+
+    @NotNull(message = "名称不能为空")
+    @Size(min = 1, max = 20, message = "分类名称字符数在1-20之间")
+    private String name;
+
+    private int order;
+
+}
---
+++ wave/wave-product/src/main/java/com/qingala/admin/product/SpuRepository.java
@@ -0,0 +1,13 @@
+package com.qingala.admin.product;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SpuRepository {
+
+    
+    private 
+
+    
+}
+ 
